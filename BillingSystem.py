50a08d1125afbd96b4bc99d18b231f7cecf6fde4
Nacho <nacho.m.fullin@gmail.com>
2024-05-19 21:16:45 -0300

    Correcciones Finales

--- BillingSystem.py
+++ BillingSystem.py
@@ -19,25 +19,30 @@ class Bill:
     def calculate_total(self):
         total = self.monthly_fee
         for call in self.calls:
-            if call.category == 'local':
-                total += self.calculate_local_cost(call)
-            elif call.category == 'national':
-                total += self.calculate_national_cost(call)
-            elif call.category == 'international':
-                total += self.calculate_international_cost(call)
+            total += self.calculate_individual_call(call)
         return total
+    
+    def calculate_individual_call(self, call):
+        cost = 0
+        if call.category == 'local':
+                cost = self.calculate_local_cost(call)
+        elif call.category == 'national':
+                cost = self.calculate_national_cost(call)
+        elif call.category == 'international':
+                cost = self.calculate_international_cost(call)
+        return cost
 
     def calculate_local_cost(self, call):
         fee = 0
         day = call.date_time.weekday()
         hour = call.date_time.hour
-        if day <= 4:    #Monday to Fridat
+        if day <= 4:    #Monday to Friday
             if 8 <= hour < 20:
                 fee = call.duration * 0.20
             else:
                 fee = call.duration * 0.10
         else:       #Saturday and Sunday
-            fee = call.duration * 0.10      #CCould be simplified in just one if since 0.10 is repeated but kept it the way it was stated in the instructions for posible changes in fees
+            fee = call.duration * 0.10      #Could be simplified in just one if since 0.10 is repeated but kept it the way it was stated in the instructions for posible changes in fees
         return fee
 
     def calculate_national_cost(self, call):
@@ -61,13 +66,16 @@ class Bill:
         return fee
 
     def print_bill(self):
+        print(f'{self.month} bill')
+        print(f'Monthly fee: {self.monthly_fee}')
+        print(f'Call list:')
         total_payment = self.calculate_total()
         for index, call in enumerate(self.calls, start=1):
-            print(f'{index}. Date: {call.date_time}, Duration: {call.duration}, Category: {call.category}, Destination: {call.destination}')
-        print(f'Total amount to pay: {total_payment}')
+            print(f'{index}. Date: {call.date_time}, Duration: {call.duration}, Category: {call.category}, Destination: {call.destination}, Cost: {round(self.calculate_individual_call(call), 2)}')
+        print(f'Total amount to pay: {round(total_payment, 2)}')
 
 # Example
-phone_bill = Bill('November', 15.00)
+phone_bill = Bill('May', 15.00)
 phone_bill.calls.append(Call(datetime(2024, 5, 17, 12, 00), 15, 'local'))
 phone_bill.calls.append(Call(datetime(2024, 5, 18, 18, 30), 5, 'local'))
 phone_bill.calls.append(Call(datetime(2024, 5, 16, 7, 10), 30, 'national', 'locality1'))
